ec952f786535862536ba0f6b58ebaeaf17b912d3
Ana Sculac <asculac@lxplus760.cern.ch>
2020-10-22 14:28:13 +0200

    Dodan komentar u elempart.cpp

--- Vjezbe_2/ElementaryParticle.cpp
+++ Vjezbe_2/ElementaryParticle.cpp
@@ -4,7 +4,7 @@ ElementaryParticle::ElementaryParticle(){};
 ElementaryParticle::~ElementaryParticle(){};
 
 void ElementaryParticle::printInfo(){
-			cout<<"name: "<< name << endl;
+			cout<<"name: "<< name << endl; //komentar
 			cout<< "mass: " << mass << endl;
 			cout << "boson: " << boson << endl;
 	
